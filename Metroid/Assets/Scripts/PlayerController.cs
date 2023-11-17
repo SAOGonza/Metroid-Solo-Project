e8b012a0bd4a370ced552f784f9ed8194015577e
SAOGonza <samuelortega86@yahoo.com>
2023-11-16 19:35:20 -0800

    Created a dashing and ball mechanic.

--- Metroid/Assets/Scripts/PlayerController.cs
+++ Metroid/Assets/Scripts/PlayerController.cs
@@ -23,6 +23,23 @@ public class PlayerController : MonoBehaviour
     // Double jump variables
     private bool canDoubleJump;
 
+    // Dash variables
+    public float dashSpeed, dashTime;
+    private float dashCounter;
+    public float waitAfterDashing;
+    private float dashRechargeCounter;
+
+    // After image variables
+    public SpriteRenderer spriteRenderer, afterImage;
+    public float afterImageLifetime, timeBetweenAfterImages;
+    private float afterImageCounter;
+    public Color afterImageColor;
+
+    // Variables for ball mode.
+    public GameObject standing, ball;
+    public float waitToBall;
+    private float ballCounter;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -36,22 +53,64 @@ public class PlayerController : MonoBehaviour
         JumpPlayer();
         ControlAnimations();
         HandleFire();
+        HandleBallMode();
     }
 
     void MovePlayer()
     {
-        // Move sideways
-        playerRb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, playerRb.velocity.y);
+        // Cooldown between dashing.
+        if (dashRechargeCounter > 0)
+        {
+            dashRechargeCounter -= Time.deltaTime;
+        }
+        else
+        {
+            // Let player dash on RMB click.
+            if (Input.GetButtonDown("Fire2") && standing.activeSelf) // Only dash if standing.
+            {
+                dashCounter = dashTime;
 
-        // Handle direction change.
-        if (playerRb.velocity.x < 0)
-        {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
+                ShowAfterImage();
+            }
         }
-        else if (playerRb.velocity.x > 0)
+
+        // Give player ability to dash for duration of dashCounter.
+        if (dashCounter > 0)
         {
-            transform.localScale = Vector3.one;
+            DashPlayer();
         }
+        else // Move player normally when not dashing.
+        {
+            // Move sideways
+            playerRb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, playerRb.velocity.y);
+
+            // Handle direction change.
+            if (playerRb.velocity.x < 0)
+            {
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+            }
+            else if (playerRb.velocity.x > 0)
+            {
+                transform.localScale = Vector3.one;
+            }
+        }  
+    }
+
+    void DashPlayer()
+    {
+        dashCounter -= Time.deltaTime;
+        playerRb.velocity = new Vector2(dashSpeed * transform.localScale.x, playerRb.velocity.y);
+
+        // Count down time between after images to create several at a time.
+        // our afterImageCounter was initialized in ShowAfterImage() function.
+        afterImageCounter -= Time.deltaTime;
+        if (afterImageCounter <= 0)
+        {
+            ShowAfterImage();
+        }
+
+        // Cooldown before dashing again.
+        dashRechargeCounter = waitAfterDashing;
     }
 
     void JumpPlayer()
@@ -77,6 +136,59 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    void HandleBallMode()
+    {
+        // Check if ball mode is not active. If not,
+        // then get our vertical inputs to become a ball.
+        if (!ball.activeSelf)
+        {
+            if (Input.GetAxisRaw("Vertical") < -.9f)
+            {
+                // Begin transition to the ball. Count down
+                // so that we become a ball if player holds button
+                // down long enough.
+                ballCounter -= Time.deltaTime;
+                if (ballCounter <= 0)
+                {
+                    // Become ball.
+                    ball.SetActive(true);
+                    standing.SetActive(false);
+                }
+            }
+            else
+            {
+                // Set counter to become ball back to full value
+                // if we let go before becoming a ball.
+                ballCounter = waitToBall;
+            }
+        }
+
+        else
+        {
+            // Ball is active, standing isn't. So we'll reverse the process
+            // to become a standing sprite.
+            if (Input.GetAxisRaw("Vertical") > .9f)
+            {
+                // Begin transition to standing. Count down
+                // so that we stand if player holds button
+                // down long enough.
+                ballCounter -= Time.deltaTime;
+                if (ballCounter <= 0)
+                {
+                    // Stand up.
+                    standing.SetActive(true);
+                    ball.SetActive(false);
+                }
+            }
+            else
+            {
+                // Set counter to become ball back to full value
+                // if we let go before standing up.
+                ballCounter = waitToBall;
+            }
+        }
+    }
+
     void ControlAnimations()
     {
         anim.SetBool("isOnGround", isOnGround);
@@ -91,5 +203,20 @@ public class PlayerController : MonoBehaviour
 
             anim.SetTrigger("shotFired");
         }
+    }
+
+    public void ShowAfterImage()
+    {
+        // Create copy of after image prefab.
+        SpriteRenderer image = Instantiate(afterImage, transform.position, transform.rotation);
+        image.sprite = spriteRenderer.sprite;
+        image.transform.localScale = transform.localScale;
+        image.color = afterImageColor;
+
+        // Delete after images.
+        Destroy(image.gameObject, afterImageLifetime);
+
+        // Set after image timer to get multiple after images instead of just one.
+        afterImageCounter = timeBetweenAfterImages;
     }
 }
